885dbe54fad2b5f97bb0b7f9c3f9935e37a409a0
Shane Wilson <shane.wilson@oicr.on.ca>
2015-01-11 00:24:40 -0500

    feat(install): moves commit-msg to data file
    Creates a hastache template for prepare-commit-msg
    
    Closes #1

--- src/CGTools/Install.hs
+++ src/CGTools/Install.hs
@@ -1,14 +1,33 @@
 module CGTools.Install (runInstall) where
 
-import Prelude hiding (writeFile, unlines)
-import Data.Text (unlines)
-import Data.Text.IO (writeFile)
+import Prelude hiding (readFile, writeFile, unlines)
+import Data.Text (unlines, Text)
+import Data.Text.IO (readFile)
+import Data.Text.Lazy.IO (writeFile)
 import System.Directory (getPermissions, setPermissions, executable)
+-- import System.FilePath.Posix ((</>))
+import Text.Hastache (MuContext, hastacheStr, defaultConfig)
+import Text.Hastache.Context (mkGenericContext)
+import Data.Data (Data, Typeable)
+import Paths_cgtools
+
+data Ctx = Ctx {
+  path :: Text
+} deriving (Data, Typeable)
 
 runInstall :: IO ()
 runInstall = do
-  writeFile commitHook $ unlines [ "#!/bin/sh", "exec < /dev/tty", ".cabal-sandbox/bin/cgtools validate"]
+  let context = mkGenericContext ctx
+  genFile context "prepare-commit-msg" commitHook
   p <- getPermissions commitHook
   setPermissions commitHook (p {executable = True})
   where
     commitHook = ".git/hooks/prepare-commit-msg"
+    ctx = Ctx { path = ".cabal-sandbox/bin/cgtools" }
+
+genFile :: MuContext IO -> FilePath -> FilePath -> IO ()
+genFile context input output = do
+    pkgfileName <- getDataFileName ("scaffold/" ++ input)
+    template <- readFile pkgfileName
+    transformedFile <- hastacheStr defaultConfig template context
+    writeFile output transformedFile
