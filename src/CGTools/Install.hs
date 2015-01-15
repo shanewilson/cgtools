7267f1106b2b97d467d1be6a450f96a617b6daf6
Shane Wilson <shane.wilson@oicr.on.ca>
2015-01-14 23:12:06 -0500

    feat(install): check if git hooks exist
    Allows user to back up git hooks
    
    Closes #4

--- src/CGTools/Install.hs
+++ src/CGTools/Install.hs
@@ -4,12 +4,15 @@ import Prelude hiding (readFile, writeFile, unlines)
 import Data.Text (Text, unpack, pack, append, strip)
 import Data.Text.IO (readFile)
 import Data.Text.Lazy.IO (writeFile)
-import System.Directory (getPermissions, setPermissions, executable)
+import Control.Monad (when)
+import System.Directory (getPermissions, setPermissions, executable, doesFileExist, renameFile)
 import System.Process (readProcess)
+import System.FilePath (splitFileName, (</>), (<.>))
 import Text.Hastache (MuContext, hastacheStr, defaultConfig)
 import Text.Hastache.Context (mkGenericContext)
 import Data.Data (Data, Typeable)
 import Paths_cgtools
+import System.IO (hFlush, stdout)
 
 data Ctx = Ctx {
   path :: Text
@@ -33,6 +36,19 @@ getPrepareCommitPath g = unpack $ strip g `append` "/hooks/prepare-commit-msg"
 
 genFile :: MuContext IO -> FilePath -> FilePath -> IO ()
 genFile context input output = do
+    exists <- doesFileExist output
+    when exists $ do
+      let (d,f) = splitFileName output
+      putStr $ "backup " ++ f ++ "? (y/n [y]): "
+      hFlush stdout
+      bkup <- getChar
+      when (bkup /= 'n') $ do
+        putStr $ "name of backup ["++ f ++".bak]: "
+        hFlush stdout
+        backup <- getLine
+        let backupName = if backup == "" then d </> f <.> "bak" else d </> backup
+        renameFile output backupName
+
     pkgfileName <- getDataFileName ("scaffold/" ++ input)
     template <- readFile pkgfileName
     transformedFile <- hastacheStr defaultConfig template context
