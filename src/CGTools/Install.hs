60c0a60526967067a161aea820d8fb9119cf4441
Shane Wilson <shane.wilson@oicr.on.ca>
2015-01-23 20:31:08 -0500

    feat(install): adds coloured output
    - adds asni module for colour output
    
    Closes #5

--- src/CGTools/Install.hs
+++ src/CGTools/Install.hs
@@ -2,29 +2,41 @@ module CGTools.Install (runInstall) where
 
 import System.Environment.FindBin (getProgPath)
 import Control.Monad (when)
+import System.Console.ANSI
 
 import CGTools.Types (CommonOpts(optVerbosity), InstallOpts(..), Verbosity(..))
 import CGTools.Install.Internal (checkDependencies, createCommitHooks, createBashCompletion)
 
-info :: Verbosity -> String -> IO ()
-info Normal _ = return ()
-info Verbose s = putStr s
+output :: ColorIntensity -> Color -> String -> Verbosity -> IO ()
+output  _ _ _ Normal  = return ()
+output  i c s Verbose = do
+  setSGR [SetColor Foreground i c]
+  putStr s
+  setSGR []
+
+
+info :: String -> Verbosity -> IO ()
+info = output Vivid White
+
+
+success :: Verbosity -> IO ()
+success = output Dull Green "OK\n"
 
 
 runInstall :: CommonOpts -> InstallOpts -> IO ()
 runInstall cOpts insOpts = do
-  info v "Checking dependencies..."
+  info "Checking dependencies..." v
   checkDependencies
-  info v "OK\n"
+  success v
   bin <- getProgPath
   let path = bin ++ "/cgtools"
-  info v "Generating Git Hooks..."
+  info "Generating Git Hooks..." v
   createCommitHooks path safety
-  info v "OK\n"
+  success v
   when bash $ do
-    info v "Generating Bash Completion..."
+    info "Generating Bash Completion..." v
     createBashCompletion path safety
-    info v "OK\n"
+    success v
   where
     bash = optCompletion insOpts
     safety = optSafety insOpts
